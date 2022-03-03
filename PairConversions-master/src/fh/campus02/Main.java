018649c797f30189221f523fbe04f2c197f8acfe
s51633 <rnawaqil@gmail.com>
2022-03-03 11:32:57 +0100

    added

--- PairConversions-master/src/fh/campus02/Main.java
+++ PairConversions-master/src/fh/campus02/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// nothing to do here, look at the instruction files!
+        //In your main method:
+        //    int kilometers
+        int kilometers;
+        //Write a method:
+        //    public static double kilometersToMiles(int kilometers)
+        //    in this method convert a kilometer value into miles (look up the formula online)
     }
+    public static
 }
