287525f81daef6481e9d09a3f989271e68d6a608
DeaExMachina <sister_of_night@gmx.at>
2022-03-03 14:01:01 +0100

    .

--- PairConversions-master/src/fh/campus02/Bookshop.java
+++ PairConversions-master/src/fh/campus02/Bookshop.java
@@ -7,14 +7,22 @@ public class Bookshop {
         int fiction = 1;
         boolean discount;
 
-    if (nonFiction > 2 && fiction >= 1) {
-        discount = true;
+        discount = getBookDiscount(nonFiction, fiction);
+
+
+        System.out.println("I will get a discount: " + discount);
+
     }
-        else {
-                discount = false;
-            }
 
-            System.out.println("I will get a discount: " + discount);
-
+    public static boolean getBookDiscount (int nonFiction, int fiction) {
+        boolean discount;
+        if (nonFiction > 2 && fiction >= 1) {
+            discount = true;
         }
+        else {
+            discount = false;
+        }
+        return discount;
     }
+}
+
