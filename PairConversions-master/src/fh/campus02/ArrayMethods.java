b4b4dad2852c3c396503d7fad6fd51fc661f9496
DeaExMachina <sister_of_night@gmx.at>
2022-03-04 12:14:17 +0100

    .

--- PairConversions-master/src/fh/campus02/ArrayMethods.java
+++ PairConversions-master/src/fh/campus02/ArrayMethods.java
@@ -4,10 +4,18 @@ public class ArrayMethods {
     public static void main(String[] args) {
         int[] schachtel = new int[] {2, 3, 5, 7, 11, 13, 17};
 
+        printArray(schachtel);
+
 
     }
 
    public static void printArray (int[] array) {
-
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.println(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
    }
 }
