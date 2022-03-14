c48969959340951e6582946c8d887945377af7ae
DeaExMachina <sister_of_night@gmx.at>
2022-03-14 11:27:24 +0100

    .

---
+++ PairConversions-master/src/ArrayDemo.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        //int [] array1 = new int [3];
+        int [] array1 = {2, 26, 9};
+        int [] array2 = {11, 4 ,17};
+
+        System.out.print(array1[0] + array2[0] + ", ");
+        System.out.print(array1[1] + array2[1] + ", ");
+        System.out.println(array1[2] + array2[2] + " ");
+
+        System.out.print(array1[0] * array2[0] + ", ");
+        System.out.print(array1[1] * array2[1] + ", ");
+        System.out.println(array1[2] * array2[2]);
+
+
+    }
+}
---
+++ PairConversions-master/src/ArrayTest.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        String [] quartale = {"Frühling", "Sommer", "Herbst", "Winter"};
+
+        // Arrays.toString erlaubt Ausgabe des Inhalts, sonst wird objektreferenz ausgegeben
+        System.out.println(Arrays.toString(quartale));
+    }
+}
