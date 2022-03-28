537ec6042898bd1d6ccb0cba98ccb8469707550d
DeaExMachina <sister_of_night@gmx.at>
2022-03-28 10:12:36 +0200

    .

--- PairConversions-master/src/smallestNumber.java
+++ PairConversions-master/src/smallestNumber.java
@@ -1,17 +1,18 @@
 public class smallestNumber {
-    public int findeKleinsteZahl (int [] numbers) {
-        int smallest = 0;
+    public static int findeKleinsteZahl (int [] numbers) {
+        int smallest = numbers[0];
         //int index = numbers[0 + 1];
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < smallest) {
-                smallest = i;
+                smallest = numbers[i]; //um den Index des Arrays zu bekommen: smallest = i --> zeigt den Wert an der Stelle im Array an
             }
         }
         return smallest;
     }
 
-    public  void main(String[] args) {
-        System.out.println(findeKleinsteZahl(10, 4, 17, 8, 2));
+    public static void main(String[] args) {
+        int [] numbers = {12, 1, 8, 6, 10, 3};
+        System.out.println(findeKleinsteZahl(numbers));
 
     }
 }
